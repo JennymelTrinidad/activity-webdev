2aa5f283998500a22350f6d0025f7ebd5f01cd23
JennymelTrinidad <trinidadjennymelc@gmail.com>
2024-11-26 15:23:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+var state = false;
+
+function Blue(){
+ var body = document.body;
+ var greet = document.getElementById('greet');
+ var info = document.getElementById('info');
+ var img = document.getElementById('img')
+
+ if(!state){
+ body.style.backgroundColor = "#365887";
+ body.style.color = "#ffffff";
+ body.style.fontFamily = ""
+ greet.innerHTML = "｡𖦹˚.🪼₊ ๋࣭ ⭑";
+ info.innerHTML = "A soul as deep and vast as the sea. I chase sunsets on the horizon, seek adventure in the waves, and find peace in the sound of the ocean.";
+ greet.style.fontFamily = "Verdana";
+ greet.style.fontWeight = "700";
+ img.style.transform = "translateY(-69%)";
+ img.src = "image/men.jpg";
+ info.style.marginLeft = "5.3%";
+ info.style.fontFamily = "Tahoma";
+ info.style.fontWeight = "450";
+ state = true;
+}
+else{
+ body.style.backgroundColor = "";
+ body.style.color = "";
+ greet.innerHTML = "Welcome!";
+ info.innerHTML = "Here, you'll dive into the world of coding, from HTML and CSS to JavaScript and beyond.";
+ greet.style.fontFamily = "";
+ greet.style.fontWeight = "";
+ img.style.transform = "translateY(-57%)";
+ img.src = "image/cat.jpg";
+ info.style.marginLeft = "";
+ info.style.marginLeft = "";
+ info.style.fontFamily = "";
+ info.style.fontWeight = "";
+ state = false;
+}
+}
+
+var state = false;
+
+function Pink(){
+ var body = document.body;
+ var greet = document.getElementById('greet');
+ var info = document.getElementById('info');
+ var img = document.getElementById('img')
+
+ if(!state){
+ body.style.backgroundColor = "#f4acb7";
+ body.style.color = "#ffffff";
+ body.style.fontFamily = ""
+ greet.innerHTML = "˚˖𓍢ִִ໋🌊 🫧 ˚˖𓍢ִ✧˚";
+ info.innerHTML = "A free spirit with the ocean's heart—adventurous, ever-changing, and at peace by the waves. I find beauty in the unpredictable and let life flow like the tides. ";
+ greet.style.fontFamily = "Fantasy";
+ greet.style.fontWeight = "500";
+ img.style.transform = "translateY(-69%)";
+ img.src = "image/women.jpg";
+ info.style.marginLeft = "5%";
+ info.style.fontFamily = "Cursive";
+ info.style.fontWeight = "500";
+ state = true;
+}
+else{
+ body.style.backgroundColor = "";
+ body.style.color = "";
+ greet.innerHTML = "Welcome!";
+ info.innerHTML = "Here, you'll dive into the world of coding, from HTML and CSS to JavaScript and beyond.";
+ greet.style.fontFamily = "";
+ greet.style.fontWeight = "";
+ img.style.transform = "translateY(-57%)";
+ img.src = "image/cat.jpg";
+ info.style.marginLeft = "";
+ info.style.marginLeft = "";
+ info.style.fontFamily = "";
+ info.style.fontWeight = "";
+ state = false;
+}
+}
+
+
+var state = false;
+
+function Brown(){
+ var body = document.body;
+ var greet = document.getElementById('greet');
+ var info = document.getElementById('info');
+ var img = document.getElementById('img')
+
+ if(!state){
+ body.style.backgroundColor = "#c6ac8f";
+ body.style.color = "#ffffff";
+ body.style.fontFamily = ""
+ greet.innerHTML = "⋆｡˚🪼🫧˚｡⋆";
+ info.innerHTML = "Together, we've weathered life's storms and sailed through calm seas. Our love, like the ocean, is timeless and boundless—forever flowing, never fading. Here's to the tides we've crossed and the waves still to come.";
+ greet.style.fontFamily = "Times";
+ greet.style.fontWeight = "700";
+ img.style.transform = "translateY(-69%)";
+ img.src = "image/old.jpg";
+ info.style.marginLeft = "5%";
+ info.style.fontFamily = "Cambria";
+ info.style.fontWeight = "500";
+ state = true;
+}
+else{
+ body.style.backgroundColor = "";
+ body.style.color = "";
+ greet.innerHTML = "Welcome!";
+ info.innerHTML = "Here, you'll dive into the world of coding, from HTML and CSS to JavaScript and beyond.";
+ greet.style.fontFamily = "";
+ greet.style.fontWeight = "";
+ img.style.transform = "translateY(-57%)";
+ img.src = "image/cat.jpg";
+ info.style.marginLeft = "";
+ info.style.marginLeft = "";
+ info.style.fontFamily = "";
+ info.style.fontWeight = "";
+ state = false;
+}
+}
